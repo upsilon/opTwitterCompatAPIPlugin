99e967a5b9f66f1ae1703300b463df5d6e4cbf32
upsilon <kim.upsilon@bucyou.net>
2011-02-14 01:33:57 +0900

    move common methods into the parent class

--- apps/api/modules/twStatuses/actions/actions.class.php
+++ apps/api/modules/twStatuses/actions/actions.class.php
@@ -2,97 +2,6 @@
 
 class twStatusesActions extends opTwitterCompatAPIActions
 {
-  public function forward404($message = null)
-  {
-    $this->forward('twApi', 'error404');
-  }
-
-  protected function validate($validators, $default = array())
-  {
-    $result = array();
-    try
-    {
-      foreach ($validators as $name => $validator)
-      {
-        $value = $this->getRequest()->getParameter($name, isset($default[$name]) ? $default[$name] : null);
-        $result[$name] = $validator->clean($value);
-      }
-    }
-    catch (sfValidatorError $e)
-    {
-      $this->forward('twApi', 'error403');
-    }
-    return $result;
-  }
-
-  protected function getMemberIdByBasic()
-  {
-    if (!isset($_SERVER['PHP_AUTH_USER']))
-    {
-      $this->forward('twApi', 'basicAuth');
-    }
-    else
-    {
-      $email = $_SERVER['PHP_AUTH_USER'];
-      $password = md5($_SERVER['PHP_AUTH_PW']);
-      $memberConfig = Doctrine::getTable('MemberConfig')->retrieveByNameAndValue('pc_address', $email);
-      if ($memberConfig)
-      {
-        $data = $memberConfig->Member->getConfig('password');
-        if ($data === $password)
-        {
-          return $memberConfig->member_id;
-        }
-      }
-      $this->forward('twApi', 'basicAuth');
-    }
-  }
-
-  protected function getMemberIdByOAuth()
-  {
-    require_once 'OAuth.php';
-    $consumer = $token = null;
-
-    try
-    {
-      $req = OAuthRequest::from_request();
-      list($consumer, $token) = $this->getServer()->verify_request($req);
-    }
-    catch (OAuthException $e)
-    {
-      // do nothing
-    }
-
-    if ($consumer)
-    {
-      $information = Doctrine::getTable('OAuthConsumerInformation')->findByKeyString($consumer->key);
-      if ($information)
-      {
-        $tokenType = $this->getRequest()->getParameter('token_type', 'member');
-        if ('member' === $tokenType)
-        {
-          $accessToken = Doctrine::getTable('OAuthMemberToken')->findByKeyString($token->key, 'access');
-          return $accessToken->member_id;
-        }
-      }
-    }
-
-    $this->forward('twApi', 'error401');
-  }
-
-  protected function getMemberId()
-  {
-    if ($this->getRequest()->hasParameter('oauth_token'))
-    {
-      return $this->getMemberIdByOAuth();
-    }
-    if (sfConfig::get('op_twcompat_use_basic_auth', false))
-    {
-      return $this->getMemberIdByBasic();
-    }
-    $this->forward('twApi', 'error401');
-  }
-
   public function executeHomeTimeline(sfWebRequest $request)
   {
     $memberId = $this->getMemberId();
@@ -110,7 +19,7 @@ class twStatusesActions extends opTwitterCompatAPIActions
       'term_user'        => false
     );
 
-    $params = $this->validate($validators, $default);
+    $params = $this->validateParameters($validators, $default);
 
     $q = Doctrine::getTable('ActivityData')->createQuery();
     $dql = 'member_id = ?';
@@ -155,7 +64,7 @@ class twStatusesActions extends opTwitterCompatAPIActions
       'status'    => new opValidatorString(array('required' => true, 'trim' => true, 'max_length' => 140)),
       'term_user' => new sfValidatorBoolean(array('required' => false)),
     );
-    $params = $this->validate($validators, array('term_user' => false));
+    $params = $this->validateParameters($validators, array('term_user' => false));
 
     $this->activity = Doctrine::getTable('ActivityData')->updateActivity($memberId, $params['status']);
     $this->term_user = $params['term_user'];
--- lib/action/opTwitterCompatAPIActions.class.php
+++ lib/action/opTwitterCompatAPIActions.class.php
@@ -18,4 +18,96 @@ class opTwitterCompatAPIActions extends sfActions
 
     return parent::execute($request);
   }
+
+  public function forward404($message = null)
+  {
+    $this->forward('twApi', 'error404');
+  }
+
+  protected function validateParameters($validators, $default = array())
+  {
+    $result = array();
+    try
+    {
+      foreach ($validators as $name => $validator)
+      {
+        $value = $this->getRequest()->getParameter($name, isset($default[$name]) ? $default[$name] : null);
+        $result[$name] = $validator->clean($value);
+      }
+    }
+    catch (sfValidatorError $e)
+    {
+      $this->forward('twApi', 'error403');
+    }
+
+    return $result;
+  }
+
+  protected function getMemberIdByBasic()
+  {
+    if (!isset($_SERVER['PHP_AUTH_USER']))
+    {
+      $this->forward('twApi', 'basicAuth');
+    }
+    else
+    {
+      $email = $_SERVER['PHP_AUTH_USER'];
+      $password = md5($_SERVER['PHP_AUTH_PW']);
+      $memberConfig = Doctrine::getTable('MemberConfig')->retrieveByNameAndValue('pc_address', $email);
+      if ($memberConfig)
+      {
+        $data = $memberConfig->Member->getConfig('password');
+        if ($data === $password)
+        {
+          return $memberConfig->member_id;
+        }
+      }
+      $this->forward('twApi', 'basicAuth');
+    }
+  }
+
+  protected function getMemberIdByOAuth()
+  {
+    require_once 'OAuth.php';
+    $consumer = $token = null;
+
+    try
+    {
+      $req = OAuthRequest::from_request();
+      list($consumer, $token) = $this->getServer()->verify_request($req);
+    }
+    catch (OAuthException $e)
+    {
+      // do nothing
+    }
+
+    if ($consumer)
+    {
+      $information = Doctrine::getTable('OAuthConsumerInformation')->findByKeyString($consumer->key);
+      if ($information)
+      {
+        $tokenType = $this->getRequest()->getParameter('token_type', 'member');
+        if ('member' === $tokenType)
+        {
+          $accessToken = Doctrine::getTable('OAuthMemberToken')->findByKeyString($token->key, 'access');
+          return $accessToken->member_id;
+        }
+      }
+    }
+
+    $this->forward('twApi', 'error401');
+  }
+
+  protected function getMemberId()
+  {
+    if ($this->getRequest()->hasParameter('oauth_token'))
+    {
+      return $this->getMemberIdByOAuth();
+    }
+    if (sfConfig::get('op_twcompat_use_basic_auth', false))
+    {
+      return $this->getMemberIdByBasic();
+    }
+    $this->forward('twApi', 'error401');
+  }
 }
